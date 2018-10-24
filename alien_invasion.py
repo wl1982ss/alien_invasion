7302cefa29e619f47202c5be349851d345fd25b2
wl1982ss <32494514@qq.com>
2018-10-24 13:23:53 +0800

    添加子弹

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -14,9 +15,12 @@ def run_game():
 
     #创建飞船
     ship = Ship(ai_settings, screen)
+    #创建一个用于存储子弹的编组
+    bullets = Group()
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 run_game()
