51c24dfc95034dbecf00ef9c4c35d3432bb8988b
wl1982ss <32494514@qq.com>
2018-10-25 20:37:18 +0800

    删除消失的子弹

--- game_functions.py
+++ game_functions.py
@@ -11,8 +11,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         # 创建换一颗子弹，并将其加入到编组bullets中
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+       fire_bullet(ai_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
     """响应松开"""
@@ -41,3 +40,20 @@ def update_screen(ai_settings, screen, ship, bullets):
     ship.blitme()
 
     pygame.display.flip()
+
+def update_bullets(bullets):
+    """更新子弹的位置，并删除已消失的子弹"""
+    # 更新子弹的位置
+    bullets.update()
+
+    # 删除已消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """如果还没有到达限制，就发射一颗子弹"""
+    #创建新子弹，并将其加入到编组 bullets 中
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
