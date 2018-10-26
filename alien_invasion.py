767fc0c80779f03ba0e45d9827786bca7a504625
wl1982ss <32494514@qq.com>
2018-10-26 20:03:06 +0800

    添加外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -13,14 +14,20 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #创建飞船
+    #创建一艘飞船、一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
     #创建一个用于存储子弹的编组
     bullets = Group()
+    aliens = Group()
+
+    #创建外星人群
+    #alien = Alien(ai_settings, screen)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 run_game()
--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings():
     def __init__(self):
         #屏幕设置
         self.screen_width = 1200
-        self.screen_height = 400
+        self.screen_height = 700
         self.bg_color = (230, 230, 230)
 
         self.ship_speed_factor = 1.5
@@ -13,3 +13,9 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
+
+        # 外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction为1表示向右移动，为-1表示向左移动
+        self.fleet_direction = 1
