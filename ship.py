5a8d050669efff3c73c4ce567e3404a831fa9e71
wl1982ss <32494514@qq.com>
2018-10-27 14:24:27 +0800

    显示得分（完整）

--- ship.py
+++ ship.py
@@ -1,7 +1,10 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship():
+class Ship(Sprite):
     def __init__(self, ai_settings, screen):
+        """初始化飞船，并设置其起始位置"""
+        super(Ship, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
